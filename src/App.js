2ac051781a907323ecbfa26502f23b1c23bce21a
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-10-19 14:22:14 +0700

    Successfully add antd 'Default Button'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TagButton from './components/TagButton';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <TagButton/>
     </div>
   );
 }
