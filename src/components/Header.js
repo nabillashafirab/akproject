bf3dca40a6334b7a060b50377e8b9162bc8e2af9
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-10-19 22:18:25 +0700

    added 'styled-components'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  background-color: #282c34;
+  min-height: 70vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: #61dafb;
+`;
 
 const Header = () => {
   return (
