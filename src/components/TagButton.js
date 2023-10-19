40caf7eeb8361662d73d4710a865115436acc2a7
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-10-19 13:17:00 +0700

    add Tag Button components

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -1,1 +1,10 @@
-// Afk makan
+import React from 'react';
+import { Button, Space } from 'antd';
+
+const TagButton = () => (
+  <Space wrap>
+    <Button>Default Button</Button>
+  </Space>
+);
+export default TagButton;
+// gini g sih?
