c781c60981f394bf2f429ea5170622be3e7d3f69
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-10-24 07:59:15 +0700

    show picture when the button pressed: done

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Space } from 'antd';
 
-const TagButton = () => (
-  <Space wrap>
-    <Button>Default Button</Button>
-  </Space>
-);
+// const TagButton = () => (
+//   <Space wrap>
+//     <Button>Default Button</Button>
+//   </Space>
+// );
+// export default TagButton;
+
+function TagButton() {
+  const [showImage, setShowImage] = useState(false);
+
+  const toggleImage = () => {
+    setShowImage(!showImage);
+  };
+
+  return (
+    <div>
+      <Button onClick={toggleImage}>Guard</Button>
+      {showImage && <img src="https://images.pexels.com/photos/18201343/pexels-photo-18201343/free-photo-of-woman-with-eyes-closed-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Placeholder" />}
+    </div>
+  );
+}
+
 export default TagButton;
-// gini g sih?
